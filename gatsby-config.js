b5f2da92b0fbb860142da325fe37c029478bb706
Billy <billydawson98@gmail.com>
2022-09-11 14:49:35 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": "https://cms.fiftyml.com/graphql"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-4RR287107Q"
+    }
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
